d32e697adea6207597a7bd9105b4068dd86acf81
dshsu <73321543+dshsu@users.noreply.github.com>
2020-12-02 23:39:05 -0500

    Beginning of rot13 project

---
+++ rot13.js
@@ -0,0 +1,9 @@
+//this is a rot-13 generator and decoder just for fun
+function rot13(input) {
+  var string=Array.from(input);
+  var alpha=[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+  var output=[];
+  for (var i = 0; i < string.length; i++) {
+    string[i]
+  }
+}
